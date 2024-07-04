68dd3f54ce9acc9c0ae7062ad404d304ea8d5a2f
sassanghz <sassanghazi@gmail.com>
2024-07-04 00:21:35 -0400

    complete

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import random
 import sqlite3
 from datetime import datetime
 
+# Character sets
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowercase_letters = uppercase_letters.lower()
 digits = "0123456789"
 symbols = "()[]{},;:.- /\\?+*# "
 
-upper, lower, nums, syms = True, True, True, True # Can set as false and the compiler will not include the specific characters in the password
+# Configuration for character inclusion
+upper, lower, nums, syms = True, True, True, True
 all = ""
 
 if upper:
@@ -19,31 +21,35 @@ if nums:
 if syms:
     all += symbols
 
-length = 8 # the length of characters in the password
-amount = 10 # the number of passwords one can generate
+length = 8  # The length of characters in the password
+amount = 10  # The number of passwords to generate
 
 # Database setup
 connect = sqlite3.connect('passwords.db')
 c = connect.cursor()
 
-# Create table if not exists
+# Drop the existing table if it exists
+c.execute('DROP TABLE IF EXISTS passwords')
+
+# Create table with new schema
 c.execute('''
     CREATE TABLE IF NOT EXISTS passwords (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
+        created TEXT NOT NULL,
+        tag TEXT
     )
 ''')
 
 # Generate passwords and store them in the database
-
 for x in range(amount):
-    passwrd = "".join(random.sample(all, length))
+    password = "".join(random.sample(all, length))
     created = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    tag = "example_tag"
-    print(passwrd)
+    tag = "example_tag" # this can be modified
+    print(password)
 
-    c.execute('Insert into passwords (password) values (?)', (passwrd,))
+    # Insert password with metadata into the database
+    c.execute('INSERT INTO passwords (password, created, tag) VALUES (?, ?, ?)', (password, created, tag))
 
 connect.commit()
 connect.close()
-    
