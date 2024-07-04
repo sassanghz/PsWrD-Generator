9a792eb4455f51880f7f097033790de931babdf4
sassanghz <sassanghazi@gmail.com>
2024-07-04 00:03:54 -0400

    tag setup

--- query.py
+++ query.py
@@ -1,10 +1,12 @@
 import sqlite3
 
+tagToSearch = "example_tag"
+
 connect = sqlite3.connect('passwords.db')
 c = connect.cursor()
 
-# Query to select all passwords 
-c.execute('Select * from passwords')
+# Query to select passwords with a specific tag
+c.execute('SELECT * FROM passwords WHERE tag=?', (tagToSearch,))
 
 # Fetch all results
 
@@ -13,6 +15,5 @@ passwords = c.fetchall()
 # Print the passwords
 
 for row in passwords:
-    print(f"ID: {row[0]}, Password: {row[1]}")
-
+    print(f"ID: {row[0]}, Password: {row[1]}, Created At: {row[2]}, Tag: {row[3]}")
 connect.close()
